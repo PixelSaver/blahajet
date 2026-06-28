6db0aa5d2a1fd540c402578e87e20f99833ebca3
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2026-06-28 01:46:02 -0700

    Working but tilts too easy

--- scenes/game/jet.gd
+++ scenes/game/jet.gd
@@ -5,39 +5,42 @@ class_name Jet
 @export var spring_strength := 1.0
 @export var spring_damping := 0.2
 @export var upright_strength := 10.
+@export var upright_damping := 0.2
 @export var raycasts: Array[RayCast3D]
 @export var mult := 1.
 
 func _physics_process(_delta: float) -> void:
 	var v = Input.get_vector("d", "a", "s", "w")
 	var b = self.global_transform.basis
+	var local_vel = b.inverse() * linear_velocity
 	
-	# Upright
-	var up = b.y
-	var axis = up.cross(Vector3.UP)
-	apply_torque(axis * upright_strength)
-	
-	
-	self.apply_central_force(-b.z * v.y * 50)
-	#self.apply_torque(b.y * v.x * 50)
-	
+	var comps = []
 	for r in raycasts:
 		r.force_raycast_update()
-		
 		if not r.is_colliding(): continue
+		
 		var hit_point := r.get_collision_point()
-		var ray_dir : Vector3 = -b.y
-		
 		var current_length := (hit_point - r.global_position).length()
-		var compression := current_length - rest_length
+		var compression := current_length - rest_length 
+		comps.append(compression)
 		if compression >= 0.0: continue
-		var offset = r.position
-		var vel = linear_velocity + angular_velocity.cross(offset)
-		var v_upwards = vel.dot(ray_dir)
-		var spring_force = compression * spring_strength
-		var damping_force = -v_upwards * spring_damping
 		
-		var total_force = ray_dir * (spring_force + damping_force)
-		self.apply_force(total_force, r.position)
+		var point_vel = linear_velocity + angular_velocity.cross(r.position)
+		var vel_along_spring := point_vel.dot(b.y)
 		
+		var spring_force = -compression * spring_strength
+		var damping_force = -vel_along_spring * spring_damping
+		
+		self.apply_force(b.y * (spring_force + damping_force), r.position)
+	print("Comps: %s" % str(comps))
+	
+	# Upright
+	var axis = b.y.cross(Vector3.UP)
+	var angular_damping := Vector3(angular_velocity.x, 0.0, angular_velocity.z) * \
+		-upright_damping
+	apply_torque(axis * upright_strength + angular_damping)
+	
+	self.apply_central_force(-b.z * v.y * 50)
+	# Bank
+	apply_torque(Vector3.UP * v.x * 10.)
 	
